628ac063c20f9a0a69781e31698be25abb1f9ae3
KevSalcedo <kevsalcedom@gmail.com>
2023-05-02 17:50:58 -0500

    Create getSuperHeroDetail Response

--- app/src/main/java/com/ketchup/superheroapp/ApiService.kt
+++ app/src/main/java/com/ketchup/superheroapp/ApiService.kt
@@ -1,5 +1,6 @@
 package com.ketchup.superheroapp
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -8,4 +9,6 @@ interface ApiService {
     @GET("10161848795970752/search/{name}")
     suspend fun getSuperheroes(@Path("name") superheroName: String): Response<SuperHeroDataResponse>
 
+    @GET("10161848795970752/access-token/{id}")
+    suspend fun getSuperheroDetail(@Path("id") superheroId: String): Response<SuperHeroDetailResponse>
 }
