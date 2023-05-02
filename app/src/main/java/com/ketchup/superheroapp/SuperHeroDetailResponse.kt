3c18ff9c7a39f6000019eb211e5e67a95fdd4e3b
KevSalcedo <kevsalcedom@gmail.com>
2023-05-02 17:45:07 -0500

    Create responses for the super Hero details

---
+++ app/src/main/java/com/ketchup/superheroapp/SuperHeroDetailResponse.kt
@@ -0,0 +1,22 @@
+package com.ketchup.superheroapp
+
+import com.google.gson.annotations.SerializedName
+
+data class SuperHeroDetailResponse(
+    @SerializedName("id") val superheroId: String,
+    @SerializedName("powerstats") val powerStats: SuperheroPowerStatsResponse,
+    @SerializedName("image") val superheroImage: SuperHeroImageDetailResponse
+)
+
+data class SuperheroPowerStatsResponse(
+    @SerializedName("intelligence") val intelligence: String,
+    @SerializedName("strength") val strength: String,
+    @SerializedName("speed") val speed: String,
+    @SerializedName("durability") val durability: String,
+    @SerializedName("power") val power: String,
+    @SerializedName("combat") val combat: String,
+)
+
+data class SuperHeroImageDetailResponse(
+    @SerializedName("image") val superheroImageDetail: String
+)
