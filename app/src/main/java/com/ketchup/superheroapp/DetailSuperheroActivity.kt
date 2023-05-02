3e53f4c84eb49458593b897cf7391d56e9a82485
KevSalcedo <kevsalcedom@gmail.com>
2023-05-02 17:49:44 -0500

    Create superHero Detail Activity

---
+++ app/src/main/java/com/ketchup/superheroapp/DetailSuperheroActivity.kt
@@ -0,0 +1,52 @@
+package com.ketchup.superheroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ketchup.superheroapp.databinding.ActivityDetailSuperheroBinding
+import com.ketchup.superheroapp.databinding.ActivitySuperHeroListBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class DetailSuperheroActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_ID = "superhero_id"
+    }
+
+    private lateinit var binding: ActivityDetailSuperheroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailSuperheroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val id = intent.getStringExtra(EXTRA_ID).orEmpty()
+        getSuperheroInformation(id)
+    }
+
+    private fun getSuperheroInformation(id: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val superheroDetail = getRetrofit().create(ApiService::class.java).getSuperheroDetail(id)
+            if (superheroDetail != null){
+                runOnUiThread {
+                    createUI(superheroDetail.body()!!)
+                }
+            }
+        }
+    }
+
+    private fun createUI(body: SuperHeroDetailResponse) {
+        TODO("Not yet implemented")
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl("https://superheroapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
